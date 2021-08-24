e4a79036f18067923c4eb0e54ed3a36f6d161f3d
ljylzc999867921 <2291014880@qq.com>
2021-08-24 13:52:55 +0800

    2021.8.23下午公司提交不能显示首页商品列表数据

---
+++ app/src/main/java/com/example/taobaounion/ui/adapter/HomePagerContentAdapter.java
@@ -0,0 +1,52 @@
+package com.example.taobaounion.ui.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.taobaounion.R;
+import com.example.taobaounion.model.domain.HomePagerContent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Date:
+Time:
+Author:lzc
+*/   public class HomePagerContentAdapter extends RecyclerView.Adapter<HomePagerContentAdapter.InnerHolder> {
+    List<HomePagerContent.DataBean> data=new ArrayList<>();
+    @NonNull
+    @Override
+    public InnerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_home_pager_content, parent, false);
+        return new InnerHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InnerHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public void setData(List<HomePagerContent.DataBean> contents) {
+        data.clear();
+        data.addAll(contents);
+        notifyDataSetChanged();
+    }
+
+    public class InnerHolder extends RecyclerView.ViewHolder {
+        public InnerHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
+
+
